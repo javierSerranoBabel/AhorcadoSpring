7d061270eba515c816c30cf5f7497f59bbc437ce
Javier Serrano <javier.serrano@babelgroup.com>
2023-03-07 12:56:14 +0100

    Initial commit

---
+++ src/main/java/com/helloworld/AhorcadoSpring/AhorcadoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.helloworld.AhorcadoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhorcadoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhorcadoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helloworld/AhorcadoSpring/AhorcadoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloworld.AhorcadoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhorcadoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
